5a72e770d01acfda8fb188937e4c5e3e9c1c899e
Greatest <joshuaejiro58@gmail.com>
2024-04-23 11:01:57 +0100

    new changes

--- playground.py
+++
@@ -1,3 +0,0 @@
-object_id = ObjectId('65e22a8271bbaebce3b63df4')
-object_id_str = str(object_id)
-print(object_id_str)
--- routes.py
+++
@@ -1,5 +0,0 @@
-from app import app
- 
-@app.route('/recommendation', methods=['GET'])
-def recommmend():
-    return 'hello'
